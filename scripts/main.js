cd022319c020c22d08736286e2cc2c6681216249
Zachary Dix <zach47d@gmail.com>
2026-06-23 23:05:42 -0500

    Add module structure and kingdom DC data

--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,21 @@ Hooks.once("ready", () => {
   console.log("Kingmaker Toolkit | Ready");
 
   game.kingmakerToolkit = {
-    version: game.modules.get("kingmaker-toolkit")?.version ?? "unknown"
+    version: game.modules.get("kingmaker-toolkit")?.version ?? "unknown",
+    apps: {},
+    services: {},
+    data: {},
+    utils: {}
   };
 
   console.log("Kingmaker Toolkit | API registered:", game.kingmakerToolkit);
+});
+
+import { KINGDOM_DC_BY_LEVEL } from "./data/kingdom-data.js";
+
+Hooks.once("ready", () => {
+  console.log(
+    "Kingmaker Toolkit | Level 8 DC:",
+    KINGDOM_DC_BY_LEVEL[8]
+  );
 });
